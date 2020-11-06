e8f2ce4a4e0981a2933d8c744dbac50bba0bde92
Eddy <eddymakboul@icloud.com>
2020-11-06 11:11:57 +0100

    push image

--- src/Main_Menu.java
+++ src/Main_Menu.java
@@ -1,7 +1,36 @@
+import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
-public class Main_Menu{
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main_Menu extends Application {
 
     private Scene scene;
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Image pacMan = new Image(new FileInputStream("C:\\Users\\eddy-\\Documents\\Master 1 Informatique\\ProjetPacman\\Image"));
+        ImageView imageView = new ImageView(pacMan);
+        imageView.setFitHeight(100);
+        imageView.setFitWidth(100);
+        Group root = new Group(imageView);
+
+        this.scene = new Scene(root, 700,700);
+        stage.setScene(scene);
+        stage.setTitle("PacMan");
+        stage.show();
+    }
+
+    public static void main(String args[]) {
+
+        Application.launch();
+    }
 
 }
