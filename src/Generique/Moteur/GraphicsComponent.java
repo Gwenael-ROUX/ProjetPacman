cb4b26a7b8080b84242fc4f824a8d0cf98c74b99
ROUX Gwenaël <37573231+Gwenael-ROUX@users.noreply.github.com>
2020-11-20 09:54:21 +0100

    Update GraphicsComponent.java

--- src/Generique/Moteur/GraphicsComponent.java
+++ src/Generique/Moteur/GraphicsComponent.java
@@ -6,6 +6,9 @@ public abstract class GraphicsComponent {
     private ImageView imageView;
     private AnimationManager animation;
 
+    public GraphicsComponent(ImageView imageView) {
+        this.imageView = imageView;
+    }
 
     abstract void update(Entity entity);
 }
