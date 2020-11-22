34716effc15d2dfe5d1f73cbb909f8493a358e10
Fievet Theo <37698979+Fievet-Theo@users.noreply.github.com>
2020-11-22 14:37:17 +0100

    update gameloop

--- src/Generique/moteur/core_kernel/GameLoop.java
+++ src/Generique/moteur/core_kernel/GameLoop.java
@@ -1,14 +1,21 @@
 package Generique.moteur.core_kernel;
 
 
+import Generique.moteur.ui.SceneGame;
+import Generique.moteur.ui.SceneMain;
 import Generique.moteur.ui.SceneManager;
+import Generique.moteur.ui.SceneManager2;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -18,24 +25,26 @@ public class GameLoop extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Pacman");
+//        stage.setTitle("Pacman");
+//
+//        Group root = new Group();
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+//
+//        //TODO initialiser scene et canvas avec scene manager
+//
+//        Canvas canvas = new Canvas( 512, 512 );
+//        root.getChildren().add(canvas);
+//
+//        GraphicsContext gc = canvas.getGraphicsContext2D();
+//        scene.setFill(Color.BLACK);
 
-        Group root = new Group();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-
-        //TODO initialiser scene et canvas avec scene manager
-
-        Canvas canvas = new Canvas( 512, 512 );
-        root.getChildren().add(canvas);
-
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        scene.setFill(Color.BLACK);
+        SceneManager2 sceneManager2 = new SceneManager2(stage, "pacman");
+        SceneMain sceneMain =  new SceneMain();
 
         final long startNanoTime = System.nanoTime();
         Timer timer = Timer.getInstance();
 
-        Image pacman = new Image(GameLoop.class.getResourceAsStream("/Image/pacman/pacmanClose.png"));
         new AnimationTimer()
         {
             public void handle(long currentNanoTime)
@@ -43,12 +52,27 @@ public class GameLoop extends Application {
                 double t = (currentNanoTime - startNanoTime) / 1000000000.0;
                 timer.setTime(t);
 
-                gameManager.update();
+                sceneManager2.stage.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent keyEvent) {
+                        if (keyEvent.getCode() == KeyCode.D)
+                            sceneManager2.setScene(new SceneGame());
+                    }
+                });
 
-                gc.drawImage(pacman, 0, 0);
+                //gameManager.update();
+
+//                gc.drawImage(pacman, 0, 0, 30,30);
+//                gc.drawImage(pacman, 470,470, 30,30);
+//                if (t > 3) {
+//                    System.out.println("c bon");
+//                    root.getChildren().remove(canvas);
+//                    Canvas canvas2 = new Canvas( 400, 400 );
+//                    root.getChildren().add(canvas2);
+//                }
             }
         }.start();
 
-        stage.show();
+        sceneManager2.show(sceneMain);
     }
 }
