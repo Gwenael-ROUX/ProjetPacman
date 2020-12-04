5817b08ffca8a50234ae06fc406cafd7a92cf482
ROUX Gwenaël <37573231+Gwenael-ROUX@users.noreply.github.com>
2020-12-04 09:07:00 +0100

    ajout des commentaires sur la composante sonore

--- src/moteur/sound/SoundManager.java
+++ src/moteur/sound/SoundManager.java
@@ -2,8 +2,12 @@ package moteur.sound;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+/**
+ * Classe permettant de gérer les son (Singleton)
+ */
 public class SoundManager {
     private static SoundManager instance;
+    // Liste contenant tous les sons en cours
     private ConcurrentLinkedQueue<Sound> currentSounds = new ConcurrentLinkedQueue<>();
 
     public static SoundManager getInstance() {
@@ -13,6 +17,14 @@ public class SoundManager {
         return instance;
     }
 
+    /**
+     * Fonction ajoutant un son aux sons courants
+     * @param soundName
+     * @param name
+     * @param isLoop
+     * @param volume
+     * @param start
+     */
     public void addSound(String soundName, String name, boolean isLoop, float volume, Long start) {
         Sound sound = new Sound(soundName, name, start);
         sound.setVolume(volume);
@@ -21,6 +33,10 @@ public class SoundManager {
         currentSounds.add(sound);
     }
 
+    /**
+     * Fonction permettant de stopper un son spécifique
+     * @param name
+     */
     public void stopASound(String name) {
         System.out.println(currentSounds.size());
         for (Sound sound : currentSounds) {
@@ -30,10 +46,17 @@ public class SoundManager {
         }
     }
 
+    /**
+     * Fonction supprimant le son de la liste des sons courants
+     * @param name
+     */
     public void removeSound(String name) {
         currentSounds.removeIf(sound -> sound.getSoundName().equals(name));
     }
 
+    /**
+     * Fonction permettant de stopper tous les sons en cours
+     */
     public void stopAllSound() {
         for (Sound sound : currentSounds) {
             sound.stopSound();
