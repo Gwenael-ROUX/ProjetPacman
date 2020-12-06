f1f755729c78e4eb9d290428f03cc7f2b435ccb7
Eddy <eddymakboul@icloud.com>
2020-12-06 18:14:10 +0100

    commentaire score + moteur UI

--- src/moteur/ui/LabelUI.java
+++ src/moteur/ui/LabelUI.java
@@ -5,9 +5,18 @@ import javafx.scene.text.Font;
 
 import java.io.InputStream;
 
+/**
+ * Classe permettant de crée graphiquement des textes
+ */
 public class LabelUI {
     Label label;
 
+    /**
+     * Initialise l'affichage du texte
+     * @param text texte à afficher
+     * @param x position du texte sur l'axe x
+     * @param y position du texte sur l'axe y
+     */
     public LabelUI(String text, double x, double y) {
         label = new Label();
         label.setText(text);
@@ -20,19 +29,28 @@ public class LabelUI {
         return label;
     }
 
+    /**
+     * Change la taille + le font du texte
+     * @param is  Font du texte
+     * @param size taille du texte
+     */
     public void changeFont(InputStream is, int size) {
         Font font = Font.loadFont(is,size);
         label.setFont(font);
     }
 
-    public void changeSize(int size){
-
-    }
-
+    /**
+     * Change la couleur du texte
+      * @param color couleur
+     */
     public void setColor(Color color) {
         label.setTextFill(color.fxcolor);
     }
 
+    /**
+     * Méthode pour update le texte
+     * @param text texte à update
+     */
     public void update(String text){
         label.setText(text);
     }
--- src/moteur/ui/SceneController.java
+++ src/moteur/ui/SceneController.java
@@ -1,11 +1,7 @@
 package moteur.ui;
 
-import gameplay.LevelGenerator;
-import javafx.scene.Node;
+
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.Region;
-import moteur.core_kernel.GameManager;
 import moteur.core_kernel.Map;
 
 public interface SceneController {
