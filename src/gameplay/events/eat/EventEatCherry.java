b8ef47b2919bf66982d08a166b4ccff049049926
Eddy <eddymakboul@icloud.com>
2020-12-06 21:53:44 +0100

    ajout des map + point aléatoire pour les cadeaux

--- src/gameplay/events/eat/EventEatCherry.java
+++ src/gameplay/events/eat/EventEatCherry.java
@@ -13,19 +13,24 @@ import moteur.sound.SoundManager;
 public class EventEatCherry extends Event {
     private PacmanModel pacmanModel;
     private Map map;
+    private final int[] scoretoadd = new int[4];
 
     public EventEatCherry(PacmanModel pacmanModel, Entity entity, Map map) {
         super(entity);
         this.pacmanModel = pacmanModel;
         this.map = map;
+        scoretoadd[0] = 1000;
+        scoretoadd[1] = 2000;
+        scoretoadd[2] = 3000;
+        scoretoadd[3] = 4000;
     }
 
     @Override
     public void handle() {
         Position position = map.getPositionEntity(entity);
         if(position == null) return;
-
-        pacmanModel.addScore(50);
+        int temp = (int) (Math.random() * 4);
+        pacmanModel.addScore(scoretoadd[temp]);
         map.deleteEntity(position, entity);
         entity.getGraphicsComponent().getCurrentImage().setImage(null);
         SoundManager.getInstance().addSound("pacman_eatfruit.wav", "gift", false, 0.2f, 0L);
